c2dcdbec71247dc6c8f8c844f8bccf9ab3495e4b
João Victor Moreira <joaovictorma21@gmail.com>
2022-05-03 21:28:44 -0300

    Create dog model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateField('Created', auto_now_add=True)
+    updated_at = models.DateField('Updated', auto_now=True)
+    is_active = models.BooleanField('Active', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Dog(Base):
+    BREEDS_CHOICES = (
+        ('Akita', 'Akita'),
+        ('American Foxhound', 'American Foxhound'),
+        ('Australian Cattle Dog', 'Australian Cattle Dog'),
+        ('Basset Hound', 'Basset Hound'),
+        ('Beagle', 'Beagle'),
+        ('Border Collie', 'Border Collie'),
+        ('Boxer', 'Boxer'),
+        ('Bulldog', 'Bulldog'),
+        ('Chihuahua', 'Chihuahua'),
+        ('Chow Chow', 'Chow Chow')
+    )
+
+    name = models.CharField('Name', max_length=100)
+    age = models.IntegerField('Age', max_length=2)
+    breed = models.CharField('Breed', max_length=100, choices=BREEDS_CHOICES)
+    image = StdImageField('Image', upload_to='dogs', variations={'thumb': (124, 124)})
+
+    class Meta:
+        verbose_name = 'Dog'
+        verbose_name_plural = 'Dogs'
+
+    def __str__(self):
+        return self.name
+
