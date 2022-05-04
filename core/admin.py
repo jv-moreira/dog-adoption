fc3863f6930ca6bcc4e5252170b56dc87a186594
João Victor Moreira <joaovictorma21@gmail.com>
2022-05-04 19:53:02 -0300

    Register Dog model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dog
 
-# Register your models here.
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age', 'breed', 'image']
