96c4b1e6d4fc080bd708b308ef7e61de97b9fc0a
João Victor Moreira <joaovictorma21@gmail.com>
2022-06-25 20:28:29 -0300

    Update settings to send emails

--- dog_adoption/settings.py
+++ dog_adoption/settings.py
@@ -135,3 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
