152d9c70cd1a5c070e13ea182d1a363c98a45f04
João Victor Moreira <joaovictorma21@gmail.com>
2022-06-25 18:46:31 -0300

    Create index and register views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Dog
+from .forms import DogModelForm
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request):
+    dogs = Dog.objects.all()
+    context = {
+        'dogs': dogs
+    }
+
+    return render(request, 'index.html', context)
+
+
+def register(request):
+    if str(request.method) == 'POST':
+        form = DogModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            dog = form.save()
+            messages.success(request, 'Dog registered!')
+        else:
+            messages.error(request, 'Error!')
+    else:
+        form = DogModelForm()
+
+    context = {'form': form}
+
+    return render(request, 'register.html', context)
